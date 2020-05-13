009492321a5b784f30beed6043003836e09cae33
D-aniela <daniela.es.ib@gmail.com>
2020-05-13 15:21:27 -0500

    Repaso Practica pagina wix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarrouselComponent } from './components/home/carrousel/carrousel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule
