1be363d5e4fd39c969431bcfdd5f4ad68f5394d3
D-aniela <daniela.es.ib@gmail.com>
2020-05-18 08:48:08 -0500

    Edicion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,18 @@ import { ElComponent } from './components/products/el/el.component';
 import { EllaComponent } from './components/products/ella/ella.component';
 import { NinosComponent } from './components/products/ninos/ninos.component';
 import { BebeComponent } from './components/products/bebe/bebe.component';
+import { ZapateriaComponent } from './components/products/zapateria/zapateria.component';
+import { SobreNosotrosComponent } from './components/Nosotros/sobre-nosotros/sobre-nosotros.component';
 
 const Rutas: Routes = [
     {path: '', component: HomeComponent},
     {path: 'el', component: ElComponent},
     {path: 'ella', component: EllaComponent},
     {path: 'ninos', component: NinosComponent},
-    {path: 'bebe', component: BebeComponent}
+    {path: 'bebe', component: BebeComponent},
+    {path: 'zapateria', component: ZapateriaComponent},
+    {path: 'nosotros', component: SobreNosotrosComponent},
+    {path: 'home', component: HomeComponent}
 ]
 
 export const APP_ROUTES = RouterModule.forRoot(Rutas);
